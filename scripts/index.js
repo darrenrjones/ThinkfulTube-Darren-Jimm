ec9b31b680779069d57ceb0de9681ecfb82b34ee
unknown <ajameslusk@gmail.com>
2018-04-10 14:31:43 -0400

    basic, single file, youtube search functioning

--- scripts/index.js
+++ scripts/index.js
@@ -32,6 +32,7 @@ const fetchVideos = function(searchTerm, callback) {
     part: 'snippet',
     key: API_KEY,
     q: searchTerm,
+    type: 'video',
   };
   $.getJSON(BASE_URL,query,callback);
 };
@@ -48,19 +49,29 @@ const decorateResponse = function(response) {
   const results = response.items.map(item => {
     return {
       id: item.id.videoId,
-      title: item.title,
-      thumbnail: item.snippet.thumbnails.medium.url
+      title: item.snippet.title,
+      thumbnail: item.snippet.thumbnails.medium.url,
     };
   });
   console.log(results);
+  console.log(results[1].title);
+  generateVideoItemHtml(results);
 };
 
 // TASK:
 // 1. Create a `generateVideoItemHtml` function that receives the decorated object
 // 2. Using the object, return an HTML string containing all the expected data
 // TEST IT!
-const generateVideoItemHtml = function(video) {
-
+const generateVideoItemHtml = function(videos) {
+  let htmlResult = '';
+  videos.forEach(video => htmlResult += `
+    <li>
+      <h3>${video.title}</h3>
+      <a href="https://www.youtube.com/watch?v=${video.id}" target="_blank"><img src="${video.thumbnail}"/></a>
+      <!--iframe width="560" height="315" src="https://www.youtube.com/embed/${video.id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe-->
+    </li>
+  `);
+  render(htmlResult);
 };
 
 // TASK:
@@ -76,8 +87,8 @@ const addVideosToStore = function(videos) {
 // 2. Map through `store.videos`, sending each `video` through your `generateVideoItemHtml`
 // 3. Add your array of DOM elements to the appropriate DOM element
 // TEST IT!
-const render = function() {
-
+const render = function(htmlResults) {
+  $('.results').html(htmlResults);
 };
 
 // TASK:
